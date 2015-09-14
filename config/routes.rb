ebb518590a695d3e28adc28b6e8f2c9e30a16e62
Jeff Gerlach <jeffgerlach06@gmail.com>
2015-09-14 04:32:18 +0000

    Moved the new section form to a modal (Lesson 34)

--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,11 @@ Flixter::Application.routes.draw do
   resources :lessons, :only => [:show]
   namespace :instructor do
     resources :lessons, :only => [:update]
-
     resources :sections, :only => [:update] do
       resources :lessons, :only => [:new, :create]
     end
     resources :courses, :only => [:new, :create, :show] do
-      resources :sections, :only => [:new, :create] 
+      resources :sections, :only => [:create] 
     end
   end
 
